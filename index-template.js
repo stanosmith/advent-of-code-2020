eaefd0c3fe45f4cf545fdda36445d9c943270c15
Stan Smith <stan.smith@planttherapy.com>
2020-12-04 18:06:05 -0700

    Create template index file which loads test input and parses it as an array

---
+++ index-template.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// https://adventofcode.com/2020/day/1
+const fs = require('fs').promises;
+// const inputPath = './input.txt';
+const inputPath = './test-input.txt';
+
+getInput()
+  .then((res) => {
+    const input = res.split('\n').filter((item) => item !== '');
+
+    console.log(input);
+    console.log('Merry Christmas! 🎄');
+  })
+  .catch((err) => console.error(err));
+
+async function getInput() {
+  const input = await fs.readFile(inputPath);
+  return input.toString();
+}
