7a9ff735d6cbe23ccf49eca08e1ee3de2d9a853c
Stan Smith <stan.smith@planttherapy.com>
2020-12-05 12:03:06 -0700

    Attempted Solve Day 3 (incorrect total `264`)

--- day03/index.js
+++ day03/index.js
@@ -1,21 +1,23 @@
 'use strict'
 
 // https://adventofcode.com/2020/day/2
+// TODO: Move the getInput function into a helpers module
 const fs = require('fs').promises
-// const inputPath = './input.txt'
-const inputPath = './test-input.txt'
+const inputPath = './input.txt'
+// const inputPath = './test-input.txt'
 
 getInput()
   .then((res) => {
     const input = res.split('\n').filter((entry) => entry !== '')
     console.log(`---\nOG input:`)
-    console.log(input)
+    console.log('input.length', input.length)
+    // console.log(input)
     console.log('---')
 
     const preppedInput = prepInput(input)
-    console.log(`Prepped input:`)
-    console.log(preppedInput)
-    console.log('---')
+    // console.log(`Prepped input:`)
+    // console.log(preppedInput)
+    // console.log('---')
 
     const solution = solvePuzzle(preppedInput)
     console.log(`OG puzzle answer ⭐️`)
@@ -38,13 +40,17 @@ async function getInput() {
 
 function prepInput(input) {
   // Using the spread operator on a String 🎩
-  return input.map((entry) => [...entry])
+  // return input.map((entry) => [...entry])
+  return input
 }
 
 function solvePuzzle(input) {
   const over = 3
   const tree = '#'
+  const hit = 'X'
+  const miss = 'O'
   let totalTreeCollisions = 0
+  let encounters = []
 
   for (let i = 0; i < input.length; i++) {
     // Calculate the next position to check
@@ -53,30 +59,34 @@ function solvePuzzle(input) {
 
     // Use the repeater to create a map line with matches the width
     const ogMapLine = input[i]
-    // TODO: Get the number repetitions we need to do in order to match the length
+    // Get the number copies needed to match the nextOver index
     const numCopies = Math.ceil(nextOver / ogMapLine.length)
-    // console.log('numCopies', numCopies)
-    const ogMapLineExtended = extendMapLine(ogMapLine, numCopies)
-    console.log(ogMapLineExtended)
+    // Create extended string
+    const mapLineExtended = ogMapLine.repeat(numCopies)
+
+    const hitTree = mapLineExtended[nextOver] === tree
+    const hitOrMiss = hitTree ? hit : miss
+    encounters.push(hitOrMiss)
+
+    console.log(ogMapLine)
+    console.log(mapLineExtended)
 
     // Find the character at the nextOver index
-    if (ogMapLineExtended[nextOver] === tree) {
+    if (hitTree) {
+      // console.log('mapLineExtended[nextOver]', mapLineExtended[nextOver])
       totalTreeCollisions++
     }
+
+    console.log('---')
   }
+
+  // console.table(encounters)
+
+  const treeCollisions = encounters.filter((encounter) => encounter === hit)
+  console.log('treeCollisions.length', treeCollisions.length)
+
+  // INFO: `264` is NOT the answer
   return totalTreeCollisions
-}
-
-function extendMapLine(mapLine, numCopies, currentCopy = 0, newMapLine = []) {
-  console.table({ numCopies, currentCopy })
-
-  if (currentCopy !== numCopies) {
-    newMapLine = [...newMapLine, ...mapLine]
-    currentCopy++
-    return extendMapLine(mapLine, numCopies, currentCopy, newMapLine)
-  }
-
-  return newMapLine
 }
 
 function solvePartTwo(input) {
