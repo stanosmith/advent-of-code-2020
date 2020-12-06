1b0954c68f316fc0da8aec28a059cf249184e127
Stan Smith <stan.smith@planttherapy.com>
2020-12-06 15:28:57 -0700

    Solve Day 4 - Part 2 ⭐️⭐️

--- day04/index.js
+++ day04/index.js
@@ -6,39 +6,57 @@ const { getInput } = require('../helpers')
 
 const inputPath = './input.txt'
 // const inputPath = './test-input.txt'
+// const inputPath = './test-input-invalid-passports.txt'
+// const inputPath = './test-input-valid-passports.txt'
 
 const passportFields = [
   {
     name: 'byr', // (Birth Year)
     ignore: false,
+    validator: byr,
   },
   {
     name: 'cid', // (Country ID)
     ignore: true,
+    validator: cid,
   },
   {
     name: 'ecl', // (Eye Color)
     ignore: false,
+    validator: ecl.bind(null, [
+      'amb',
+      'blu',
+      'brn',
+      'gry',
+      'grn',
+      'hzl',
+      'oth',
+    ]),
   },
   {
     name: 'eyr', // (Expiration Year)
     ignore: false,
+    validator: eyr,
   },
   {
     name: 'hcl', // (Hair Color)
     ignore: false,
+    validator: hcl,
   },
   {
     name: 'hgt', // (Height)
     ignore: false,
+    validator: hgt,
   },
   {
     name: 'iyr', // (Issue Year)
     ignore: false,
+    validator: iyr,
   },
   {
     name: 'pid', // (Passport ID)
     ignore: false,
+    validator: pid,
   },
 ]
 
@@ -61,11 +79,11 @@ getInput(inputPath)
     console.log(solution)
     console.log('---')
 
-    // const solutionPartTwo = solvePartTwo(preppedInput)
-    // console.log('---')
-    // console.log(`Part two puzzle answer ⭐️`)
-    // console.log(solutionPartTwo)
-    // console.log('---')
+    const solutionPartTwo = solvePartTwo(preppedInput)
+    console.log('---')
+    console.log(`Part two puzzle answer ⭐️`)
+    console.log(solutionPartTwo)
+    console.log('---')
 
     console.log('Merry Christmas! 🎄')
   })
@@ -96,11 +114,17 @@ function solvePuzzle(input) {
 }
 
 function solvePartTwo(input) {
-  const validatedPassports = getValidPassports(input).map((entry) => {
-    return passportFields
-      .filter((fieldMeta) => entry.hasOwnProperty(fieldMeta.name))
-      .map((fieldMeta) => fieldMeta.validator(entry[fieldMeta.name]))
-  })
+  const validatedPassports = getValidPassports(input)
+    .map((entry) => {
+      return (
+        passportFields
+          // .filter((fieldMeta) => entry.hasOwnProperty(fieldMeta.name))
+          .map((fieldMeta) => fieldMeta.validator(entry[fieldMeta.name]))
+      )
+    })
+    .filter((validations) => !validations.includes(false))
+
+  console.log(validatedPassports)
 
   return validatedPassports.length
 }
@@ -127,12 +151,12 @@ function getValidPassports(input) {
           .filter((fieldName) => !ignoredFields.includes(fieldName))
         console.log('missingRequiredFields', missingRequiredFields)
 
-      if (missingRequiredFields.length === 0) {
-        return {
-          ...entry,
-          valid: true,
+        if (missingRequiredFields.length === 0) {
+          return {
+            ...entry,
+            valid: true,
+          }
         }
-      }
 
         return entry
       })
@@ -140,10 +164,76 @@ function getValidPassports(input) {
   )
 }
 
-  // `201` is too high, but `200` is not… Off by one, that's crazy!
-  return validPassports.length
+/*
+|🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄
+| Validation functions
+|🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄
+*/
+
+function byr(value) {
+  console.log('byr', value)
+  // (Birth Year) - four digits; at least 1920 and at most 2002.
+  const birthYear = parseInt(value)
+  return value.length === 4 && birthYear >= 1920 && birthYear <= 2002
 }
 
-function solvePartTwo(input) {
-  return 0
+function cid(value) {
+  // cid (Country ID) - ignored, missing or not.
+  console.log('cid', value)
+  return true
+}
+
+function ecl(validValues, value) {
+  console.log('ecl value', value)
+  // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  return validValues.indexOf(value) !== -1
+}
+
+function eyr(value) {
+  console.log('eyr', value)
+  // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  const expYear = parseInt(value)
+  return value.length === 4 && expYear >= 2020 && expYear <= 2030
+}
+
+function hcl(value) {
+  console.log('hcl', value)
+  // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  const startsWithPound = value.slice(0, 1) === '#'
+  const hasSixCharacters = value.length - 1 === 6
+  const onlyValidCharacters = new RegExp(/[0-9]|[a-f]/, 'i').test(
+    value.slice(1),
+  )
+  // console.log(`onlyValidCharacters ${value.slice(1)}`, onlyValidCharacters)
+  return startsWithPound && hasSixCharacters && onlyValidCharacters
+}
+
+function hgt(value) {
+  console.log('hgt', value)
+  // hgt (Height) - a number followed by either cm or in:
+  //  - If cm, the number must be at least 150 and at most 193.
+  //  - If in, the number must be at least 59 and at most 76.
+  const units = value.substr(-2)
+  const hasValidUnits = units === 'cm' || units === 'in'
+  const height = parseInt(value.substr(0, value.length - 2))
+  const withinRange =
+    units === 'cm'
+      ? height >= 150 && height <= 193
+      : height >= 59 && height <= 76
+  // console.log('hgt height', height)
+  // console.log(`hgt ${height} withinRange`, withinRange)
+  return hasValidUnits && withinRange
+}
+
+function iyr(value) {
+  console.log('iyr', value)
+  // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  const year = parseInt(value)
+  return value.length === 4 && year >= 2010 && year <= 2020
+}
+
+function pid(value) {
+  console.log('pid with RegExp', value)
+  // pid (Passport ID) - a nine-digit number, including leading zeroes.
+  return new RegExp(/^\d{9}$/).test(value)
 }
