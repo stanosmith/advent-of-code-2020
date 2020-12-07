ce9a9235db82d4782db1e4af9646dc7369b520f4
Stan Smith <stan.smith@planttherapy.com>
2020-12-06 17:19:45 -0700

    Day 4 - decode row

--- day05/index.js
+++ day05/index.js
@@ -7,6 +7,11 @@ const { getInput } = require('../helpers')
 // const inputPath = './input.txt'
 const inputPath = './test-input.txt'
 
+const totalSeatRows = 128 // numbered 0 through 127
+const totalSeatColumns = 8 // numbered 0 through 7
+const upper = 'B'
+const lower = 'F'
+
 getInput(inputPath)
   .then((res) => {
     const input = res.split('\n').filter((entry) => entry !== '')
@@ -20,11 +25,11 @@ getInput(inputPath)
     console.log(preppedInput)
     console.log('---')
 
-    // const solution = solvePuzzle(preppedInput)
-    // console.log('---')
-    // console.log(`OG puzzle answer ⭐️`)
-    // console.log(solution)
-    // console.log('---')
+    const solution = solvePuzzle(preppedInput)
+    console.log('---')
+    console.log(`OG puzzle answer ⭐️`)
+    console.log(solution)
+    console.log('---')
     //
     // const solutionPartTwo = solvePartTwo(preppedInput)
     // console.log('---')
@@ -45,9 +50,67 @@ function solvePuzzle(input) {
   // BFFFBBFRRR: row 70, column 7, seat ID 567.
   // FFFBBBFRRR: row 14, column 7, seat ID 119.
   // BBFFBBFRLL: row 102, column 4, seat ID 820.
-  return 0
+  return input.map(decodeSeat)
 }
 
 function solvePartTwo(input) {
   return 0
+}
+
+function decodeSeat(value) {
+  const row = decodeRow(value.slice(0, 7))
+  const col = decodeCol(value.substr(7))
+  const id = getSeatId(row, col)
+
+  // INFO: decoding `FBFBBFFRLR` reveals that it is the seat at row `44`, column `5`, ID `357`
+  return {
+    id,
+    row,
+    col,
+  }
+}
+
+function decodeRow(value) {
+  // INFO: For example, consider just the first seven characters of `FBFBBFFRLR` (FBFBBFF):
+  // - Start by considering the whole range, rows 0 through 127.
+  // - F means to take the lower half, keeping rows 0 through 63.
+  // - B means to take the upper half, keeping rows 32 through 63.
+  // - F means to take the lower half, keeping rows 32 through 47.
+  // - B means to take the upper half, keeping rows 40 through 47.
+  // - B keeps rows 44 through 47.
+  // - F keeps rows 44 through 45.
+  // - The final F keeps the lower of the two, row 44.
+  value = 'FBFBBFF' // TODO: Comment after testing
+
+  return [...value.toUpperCase()].reduce(getSeat, createRange(totalSeatRows))
+}
+
+function decodeCol(value) {
+  // INFO: For example, consider just the last 3 characters of `FBFBBFFRLR` (RLR):
+  value = 'RLR'
+  // - Start by considering the whole range, columns 0 through 7.
+  // - R means to take the upper half, keeping columns 4 through 7.
+  // - L means to take the lower half, keeping columns 4 through 5.
+  // - The final R keeps the upper of the two, column 5.
+  return 0
+}
+
+function getSeat(seat, upperOrLower) {
+  const halfIndex = seat.length / 2
+  if (upperOrLower === upper) {
+    return seat.slice(halfIndex)
+  }
+  return seat.slice(0, halfIndex)
+}
+
+function createRange(max, min = 0) {
+  let range = []
+  for (let i = min; i < max; i++) {
+    range.push(i)
+  }
+  return range
+}
+
+function getSeatId(row, col) {
+  return row * totalSeatColumns + col
 }
