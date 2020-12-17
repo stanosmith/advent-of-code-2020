2543a0df4443e8b6ffd3a795bb3a4ed83b3be924
Stan Smith <stan.smith@planttherapy.com>
2020-12-16 21:52:01 -0700

    WIP Day 11 - Setup occupied left and right calculations

--- day11/index.js
+++ day11/index.js
@@ -6,8 +6,8 @@ const { getInput } = require('../helpers')
 
 // https://adventofcode.com/2020/day/11
 
-const inputPath = './input.txt'
-// const inputPath = './test-input.txt'
+// const inputPath = './input.txt'
+const inputPath = './test-input.txt'
 
 const seatFloor = '.'
 const seatEmpty = 'L'
@@ -27,15 +27,15 @@ getInput(inputPath)
     console.log('preppedInput.length', preppedInput.length)
     console.log(preppedInput)
 
-    console.log('---')
-    const solution = solvePuzzle(preppedInput)
-    console.log(`OG puzzle answer ⭐️`)
-    console.log(solution)
-
     // console.log('---')
-    // const solutionPartTwo = solvePartTwo(preppedInput, solution)
-    // console.log(`Part two puzzle answer ⭐️⭐️`)
-    // console.log(solutionPartTwo)
+    // const solution = solvePuzzle(preppedInput)
+    // console.log(`OG puzzle answer ⭐️`)
+    // console.log(solution)
+
+    console.log('---')
+    const solutionPartTwo = solvePartTwo(preppedInput)
+    console.log(`Part two puzzle answer ⭐️⭐️`)
+    console.log(solutionPartTwo)
 
     console.log('---')
     console.log('Merry Christmas! 🎄')
@@ -51,24 +51,24 @@ getInput(inputPath)
 */
 function solvePuzzle(input) {
   // How many seats end up occupied?
-  return recursive(input).reduce((occupiedSeats, seatRow) => {
-    debugger
-    return (
-      occupiedSeats + seatRow.filter((seat) => seat === seatOccupied).length
-    )
-  }, 0)
+  return recursive(input, applyRules).reduce(calculateTotal, 0)
 }
 
-function recursive(prevResult) {
-  const nextResult = applyRules(prevResult)
+function calculateTotal(occupiedSeats, seatRow) {
+  return occupiedSeats + seatRow.filter((seat) => seat === seatOccupied).length
+}
+
+function recursive(prevResult, applyRulesMethod) {
+  const nextResult = applyRulesMethod(prevResult)
+
+  console.table(prevResult)
+  console.table(nextResult)
 
   if (_.isEqual(prevResult, nextResult)) {
-    debugger
     return nextResult
   }
 
-  debugger
-  return recursive(nextResult)
+  return recursive(nextResult, applyRulesMethod)
 }
 
 function applyRules(seatingArea) {
@@ -83,7 +83,6 @@ function applyRules(seatingArea) {
       const centerSeats = getSeatGroup(seatIndex, seatRow, true)
       const bottomSeats = getSeatGroup(seatIndex, allSeatRows[rowIndex + 1])
       const adjacentSeats = [...topSeats, ...centerSeats, ...bottomSeats].flat()
-      // const adjacentSeats = [topSeats, centerSeats, bottomSeats]
       const numOccupiedAdjacentSeats = adjacentSeats.filter((adjacentSeat) => {
         return adjacentSeat === seatOccupied
       }).length
@@ -126,7 +125,76 @@ function getSeatGroup(seatIndex, seatRow, excludeCenter) {
 |
 */
 function solvePartTwo(input) {
-  return 0
+  // How many seats end up occupied?
+  return recursive(input, applyRulesPartTwo).reduce(calculateTotal, 0)
+}
+
+function applyRulesPartTwo(seatingArea) {
+  return seatingArea.map((seatRow, rowIndex, allSeatRows) => {
+    return seatRow.map((seat, seatIndex, allSeats) => {
+      // Floor seats don't change
+      if (seat === seatFloor) {
+        return seat
+      }
+
+      const visibleOccupiedSeats = getVisibleOccupiedSeats(
+        seatIndex,
+        allSeats,
+        rowIndex,
+        allSeatRows,
+      )
+      const numOccupiedAdjacentSeats = visibleOccupiedSeats.filter(
+        (adjacentSeat) => {
+          return adjacentSeat === seatOccupied
+        },
+      ).length
+
+      // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+      if (seat === seatEmpty && numOccupiedAdjacentSeats === 0) {
+        return seatOccupied
+      }
+
+      // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+      if (seat === seatOccupied && numOccupiedAdjacentSeats >= 5) {
+        return seatEmpty
+      }
+
+      return seat
+    })
+  })
+}
+
+function getVisibleOccupiedSeats(seatIndex, allSeats, rowIndex, allSeatRows) {
+  const occupiedRight = findSeatInArray(seatOccupied, allSeats)
+  const occupiedBottomRight = []
+  const occupiedBottomCenter = []
+  const occupiedBottomLeft = []
+  const occupiedLeft = getOccupiedLeft(seatIndex, allSeats)
+  const occupiedTopLeft = []
+  const occupiedTopCenter = []
+  const occupiedTopRight = []
+  return [
+    occupiedRight,
+    occupiedBottomRight,
+    occupiedBottomCenter,
+    occupiedBottomLeft,
+    occupiedLeft,
+    occupiedTopLeft,
+    occupiedTopCenter,
+    occupiedTopRight,
+  ]
+}
+
+function getOccupiedLeft(seatIndex, allSeats) {
+  if (seatIndex > 0) {
+    const seatsToSearch = allSeats.slice(0, seatIndex - 1).reverse()
+    return findSeatInArray(seatOccupied, seatsToSearch)
+  }
+  return undefined
+}
+
+function findSeatInArray(seat, seats) {
+  return seats.find((item) => item === seat)
 }
 
 /*
